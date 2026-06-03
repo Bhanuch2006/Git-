5236a909df8e9da37f0786cc1704747708a0665a
Pratyush Tayal <pratyushtayal05@gmail.com>
2026-06-03 16:45:07 +0530

    Update

--- Assets/Pratyush.cs
+++ Assets/Pratyush.cs
@@ -5,7 +5,7 @@ public class Pratyush : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("My name is pratyush, I am a student");
+        Debug.Log("My name is Pratyush");
     }
 
     // Update is called once per frame
